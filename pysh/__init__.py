3ebf56292da1827480ddf167027dcc5bf1f400c4
Greg Price <gnprice@gmail.com>
2019-07-28 21:57:44 -0700

    naming: Write down intended import styles; fix one deviation.
    Ultimately we'd want to find a way to make the preferred style
    also be the obvious one; but this will do for now while still
    experimenting with what the API should even look like.

--- pysh/__init__.py
+++ pysh/__init__.py
@@ -1,12 +1,15 @@
 
+# TODO better organize to reflect style `pysh.foo` vs `from pysh import foo`
+
 # Utilities for running external commands.
-from .words import shwords
-from .filters import slurp
-from .subprocess import (
+from .words import shwords  # style: from pysh import shwords
+from .filters import slurp  # style: pysh.slurp
+from .subprocess import (  # style: from pysh import ...
     check_cmd, check_cmd_f, slurp_cmd, slurp_cmd_f,
     try_cmd, try_cmd_f, try_slurp_cmd, try_slurp_cmd_f,
 )
 
 # Decorators for "shell builtins".
-from .filters import filter, input, output, argument, option
+from .filters import filter, input, output, argument, option  # style: pysh....
 
+# No `from .cmd import ...`; instead, style: from pysh import cmd
