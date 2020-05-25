8e348586aca23ca3dcd78b190413f49b445e5830
Greg Price <gnprice@gmail.com>
2020-05-24 17:50:50 -0700

    doc: Munge sys.path so autodoc works in a pristine environment
    Found when trying to run a build on readthedocs.org.
    
    The reason it'd been working for me is... in my normal Python
    environment for command-line use, I have site-packages/pysh as a
    symlink to my Pysh worktree.  And my sphinx-build is installed in the
    same normal Python environment.

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('_ext'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
